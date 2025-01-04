c7e6ecb3ed4c13de7c704840600ee18a9790dcef
Parjanya7 <parjanyapatel130@gmail.com>
2025-01-04 22:29:21 +0530

    Task: 2 - Allow the Add method to handle an unknown amount of numbers

--- __tests__/calculator/StringCalculator.spec.ts
+++ __tests__/calculator/StringCalculator.spec.ts
@@ -18,4 +18,10 @@ describe('StringCalculator', () => {
     it('should return sum for two numbers separated by comma', () => {
         expect(calculator.add('1,2')).toBe(3);
     });
+
+    it('should return sum for multiple numbers separated by comma', () => {
+        expect(calculator.add('1,2,3,4,5')).toBe(15);
+        expect(calculator.add('10,20,30,40')).toBe(100);
+        expect(calculator.add('1,2,3,4,5,6,7,8,9,10,11,12,13,14,15')).toBe(120);
+    });
 });
